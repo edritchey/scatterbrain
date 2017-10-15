bad65e9bca98d17053a75bd4d9a8d1c186f8d5d5
ed_ri <ed_ri@SCATTERBRAINSOL>
2017-10-14 23:55:03 -0700

    Add project files.

---
+++ Scatterbrain/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Scatterbrain
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startupLabel = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // startupLabel
+            // 
+            this.startupLabel.AutoSize = true;
+            this.startupLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startupLabel.Location = new System.Drawing.Point(74, 69);
+            this.startupLabel.Name = "startupLabel";
+            this.startupLabel.Size = new System.Drawing.Size(19, 21);
+            this.startupLabel.TabIndex = 2;
+            this.startupLabel.Text = "...";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(78, 144);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(964, 316);
+            this.listBox1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 551);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.startupLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label startupLabel;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Scatterbrain/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scatterbrain
+{
+    public partial class Form1 : Form
+    {
+        public static string sysDir = Convert.ToString(Environment.SystemDirectory);
+        public static string system = Convert.ToString(Environment.SpecialFolder.System);
+        public static string startup = Convert.ToString(Environment.SpecialFolder.Startup);
+        public static string appStartupPath = Application.StartupPath;
+        public static string myDocuments = Convert.ToString(Environment.SpecialFolder.MyDocuments);
+        public static string myDesktop = Convert.ToString(Environment.SpecialFolder.Desktop);
+        public static string progFiles64 = Convert.ToString(Environment.SpecialFolder.ProgramFiles);
+        public static string progFiles86 = Convert.ToString(Environment.SpecialFolder.ProgramFilesX86);
+        public static string currentDir = Convert.ToString(Environment.CurrentDirectory);
+        public static string sysRoot = Directory.GetDirectoryRoot(system);
+        public static string currentDirRoot = Directory.GetDirectoryRoot(currentDir);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sourceDll = currentDir + "\\AutoHotkey.dll";
+            string destDll = sysDir + "\\AutoHotkey.dll";
+
+            
+
+            if (!File.Exists(destDll))
+            {
+                
+
+                MessageBox.Show("the dll is not in the system directory");
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(sourceDll);
+                    AppDomain.CurrentDomain.Load(assembly.GetName());
+                    Type t = assembly.GetType("typeName");
+                    File.Copy(sourceDll, destDll);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    throw;
+                }
+            }
+
+
+
+
+
+            //if (File.Exists(sysDir + "\\AutoHotKey.dll"))
+            //{
+            //    MessageBox.Show(sysDir + "\\AutoHotKey.dll");
+            //}
+
+
+
+
+            string[] files = Directory.GetFileSystemEntries(currentDir);
+
+            foreach (string d in files)
+            {
+                listBox1.Items.Add(d);
+            }
+        }
+    }
+}
